87e0c832996fbca02de5f15e3fd25781d167a6ca
zorancuc <cuc.zoran@gmail.com>
2020-08-21 14:59:12 +0200

    Governance CMS Backend

--- services/firebase/firebase.js
+++ services/firebase/firebase.js
@@ -4,14 +4,14 @@ require('firebase/database');
 // this config is being used for both development and production environment. Though, it is a best practice creating a second project and have two configs: one for production (prodConfig) and another for development (devConfig), so you choose the config based on the environment.
 
 const config = {
-    apiKey: "AIzaSyDbPG-hPEgt745jbWqhi9szepQAHiUbJEQ",
-    authDomain: "cms-gov-aea3e.firebaseapp.com",
-    databaseURL: "https://cms-gov-aea3e.firebaseio.com",
-    projectId: "cms-gov-aea3e",
-    storageBucket: "cms-gov-aea3e.appspot.com",
-    messagingSenderId: "361411567573",
-    appId: "1:361411567573:web:3bd948ac29c04b3c84e75c",
-    measurementId: "G-VNJKQT61WC"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 if (!firebase.apps.length) {
